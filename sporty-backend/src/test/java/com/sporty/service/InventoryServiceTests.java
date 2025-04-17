94e62b3a06ce718b805a1ff822a25f81a38c020c
Asdrúbal Costa <asdrubal.costa@coro.net>
2025-04-17 11:39:07 +0100

    Fixing compile issue

--- sporty-backend/src/test/java/com/sporty/service/InventoryServiceTests.java
+++ sporty-backend/src/test/java/com/sporty/service/InventoryServiceTests.java
@@ -3,11 +3,9 @@ package com.sporty.service;
 import com.sporty.controller.dto.BookPageResponse;
 import com.sporty.controller.dto.BookResponse;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
